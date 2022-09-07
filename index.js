9216fb3b19f49bbda35bdc2b1c2ba354ba421354
Muriithi Gakuru <antony123muriithi@gmail.com>
2022-09-08 02:32:07 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+
+class Cat{
+    constructor(catName, gender){
+        this.catName = catName;
+        this.gender = gender;
+    }
+    speak(){
+        return `${this.catName} says meow!`
+    }
+}
+
+class Dog{
+    constructor(dogName, gender){
+        this.dogName = dogName;
+        this.gender = gender;
+    }
+    speak(){
+        return `${this.dogName} says woof!`
+    }
+}
+
+class Bird{
+    constructor(birdName, gender){
+        this.birdName = birdName;
+        this.gender = gender;
+    }
+    speak(){
+        if (this.gender === "male"){
+            return `It's me! ${this.birdName}, the parrot!`
+        }
+
+        return `${this.birdName} says squawk!`
+    }
+}
